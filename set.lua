f1bd302bba869b8cba908c84bc4dd7074092a543
Aryan Todi <aryantodi13@gmail.com>
2024-12-21 11:19:38 +0530

    initial commit

---
+++ set.lua
@@ -0,0 +1,30 @@
+vim.opt.guicursor = ""
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ruler = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+--vim.cmd("colorscheme slate")
+--vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme kanagawa-lotus")
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
